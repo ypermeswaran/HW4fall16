a5c0faa5e2e3bd95132679df7d702c264d53c255
Yashila Permeswaran <yashila-permeswaran@uiowa.edu>
2016-09-28 18:48:50 +0000

    modified redirect in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
   def create
     @user = User.create_user!(user_params)
     flash[:notice] = "Welcome #{@user.user_id}. Your account has been created."
-    redirect_to users_path
+    redirect_to movies_path
   end
 
   def edit
